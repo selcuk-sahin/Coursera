8d1e63f3557f702c06efe0627f9088cd2752ebf5
selcuk-sahin <ajanturkey@gmail.com>
2020-09-22 12:34:44 +0300

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MenuComponent } from '../app/menu/menu.component';
+import { DishdetailComponent } from '../app/dishdetail/dishdetail.component';
+import { HomeComponent } from '../app/home/home.component';
+import { AboutComponent } from '../app/about/about.component';
+import { ContactComponent } from '../app/contact/contact.component';
+
+const routes: Routes = [
+  { path: 'home',  component: HomeComponent },
+  { path: 'menu',     component: MenuComponent },
+  { path: 'about',     component: AboutComponent },
+  { path: 'contact',     component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
