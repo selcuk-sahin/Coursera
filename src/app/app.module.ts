5375072ead7eb712b3a7c3340d546116ea998330
selcuk-sahin <71325531+selcuk-sahin@users.noreply.github.com>
2020-09-23 15:51:57 +0300

    Reactive Forms, Contact Us pt2.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule}  from '@angular/material/input';
 import { MatFormFieldModule }  from '@angular/material/form-field';
 import { MatCheckboxModule }  from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
 import 'hammerjs';
@@ -49,6 +52,7 @@ import { LoginComponent } from './login/login.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    FormsModule,
     MatToolbarModule,
     MatMenuModule,
     MatIconModule,
@@ -60,7 +64,9 @@ import { LoginComponent } from './login/login.component';
     MatInputModule,
     MatFormFieldModule,
     MatCheckboxModule,
-    FormsModule
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule
   ],
   providers: [
     DishService ,
